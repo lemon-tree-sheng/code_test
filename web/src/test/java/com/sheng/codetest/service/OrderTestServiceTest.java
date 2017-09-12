516989b720748697521f8b02c315d6f5ff7a7b4a
shengxingyue <shengxingyue@souche.com>
2017-09-12 18:57:06 +0800

    simple update

--- web/src/test/java/com/sheng/codetest/service/OrderTestServiceTest.java
+++ web/src/test/java/com/sheng/codetest/service/OrderTestServiceTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * Created by shengxingyue on 2017/9/12.
  * spring-test 用于单元测试，因为要加载 spring 配置文件，所以测试用例要放在 web 项目中，再加上 @Runwith 和 @ContextConfiguration
- * 注解皆可以了。
+ * 注解就可以了。
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath*:applicationContext.xml")
